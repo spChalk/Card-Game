5af5b73abc2202cc5e1f1d872a172ad710c5bd6b
Harry <sdi1800105@di.uoa.gr>
2019-12-28 01:36:42 +0200

    checkWinCond() ready

--- gamePublic.cpp
+++ gamePublic.cpp
@@ -2,6 +2,7 @@
 // [Χάρης] Note: σου προτείνω να γράψεις εδώ τα public functions
 // της κλάσης Game και ξεχωριστά τα phases σου αλλά ό,τι 
 // θες γενικά
+#include <cstddef> //size_t
 #include <iostream>
 
 #include "baseClasses.h"
@@ -31,6 +32,33 @@ void Game::gameplay(void)
   }
 
   Player *winner = players->at(gameStatus-1);
-  std::cout << "Player \'" << winner->getName() 
+  std::cout << "Player \'" << winner->getUserName() 
             << "\' just won the game!" << std::endl;
+}
+
+/* Checks whether we have a winner                  *
+ * Returns his place in the vector counting from 1  *
+ * If no winner is found, returns 0                 */
+size_t Game::checkWinningCondition(void)
+{
+  bool playersWithProvinces = 0;
+  size_t pos;
+  size_t ctr = 1;
+
+  for (auto i : *players) // if it doesn't work, it's this cr p
+  {                       // cuz idk what i'm doing, auto is lazy af
+    auto provinces = i->getProvinces();
+    if (provinces->size() != 0)     /* If the player still has prov */
+    { 
+      if (playersWithProvinces == 1)  
+        return NO_WINNER;    /* We already found another player with prov */
+      else
+      {
+        playersWithProvinces = 1;
+        pos = ctr;     /* Keep his position in the vector */
+      }
+    }
+    ++ctr;
+  }
+  return pos;
 }
