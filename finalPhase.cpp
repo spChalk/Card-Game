090f19a10a70ae8152b252c0dcd9c313fe7c1d0e
spChalk <sdi1800209@di.uoa.gr>
2019-12-30 17:10:58 +0200

    Made finalPhase more interactive

--- finalPhase.cpp
+++ finalPhase.cpp
@@ -4,9 +4,29 @@
 #include "baseClasses.h"
 
 static void discardSurplusFateCards (Player * pl , size_t numOfCards) {
-    //TODO : kalutera na valw na kanei discard tis cards me to ligotero sumferon
-    //              maybe a function pou mageireuei attack,defence & minHonor
-    while (numOfCards--) pl->getHand()->pop_back();
+  std::cout << "Player : " << pl->getUserName() << " has to discard "<< numOfCards <<" Fate Cards !" << std::endl;
+  
+    std::cout << "Type 'Y' (YES) or '<any other key>' (NO) after each card's \
+    appearance, to discard it." << std::endl;
+
+  for (auto * i : *(pl->getHand())) {
+    
+    if (numOfCards == 0) {
+      std::cout << "Discarded Surplus Fate Cards !" << std::endl;
+      return;
+    } 
+
+    i->print();
+    std::cout << std::endl <<"Discard ? "<< std::endl << "> Your answer: " ;
+    std::string answer;
+    std::getline(std::cin, answer);
+    std::cout << answer << std::endl;
+
+    if (answer == "Y") { 
+      pl->getHand()->pop_back();
+      std::cout << "Cards remaining : " << numOfCards-- << std::endl;
+    }
+  }
 }
 
 void Game::finalPhase(Player * pl) {
