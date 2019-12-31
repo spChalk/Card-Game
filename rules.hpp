a7724eddeda996fb7e84b81d87514884581ad71a
spChalk <sdi1800209@di.uoa.gr>
2019-12-31 14:11:09 +0200

    Added General Defines

---
+++ rules.hpp
@@ -0,0 +1,60 @@
+//=====|| DEFINES ||=====
+
+//=====|| MAX CARDS IN GAME ||=====
+
+#define DECK_SIZE         40
+#define MAX_DECK_SIZE     60
+#define MAX_HAND_CARDS    7
+
+#define NO_PERSONALITIES  25
+
+#define NO_DEFENSIVE      10
+#define NO_ATTACKING      10
+#define NO_SHOGUN         2
+#define NO_CHANCELLOR     2
+#define NO_CHAMPION       1
+
+#define NO_HOLDINGS       17
+
+#define NO_PLAIN          3
+#define NO_MINE           3
+#define NO_GOLD_MINE      3
+#define NO_CRYSTAL_MINE   3
+#define NO_FARMS          3
+#define NO_SOLO           2
+
+#define NO_FOLLOWERS      28
+
+#define NO_FOOTSOLDIER    6
+#define NO_ARCHER         6
+#define NO_SIEGER         4
+#define NO_CAVALRY        5
+#define NO_NAVAL          5
+#define NO_BUSHIDO        2
+
+#define NO_ITEMS          12
+
+#define NO_KATANA         4
+#define NO_SPEAR          3
+#define NO_BOW            2
+#define NO_NINJATO        2
+#define NO_WAKIZASHI      1
+
+
+//=====|| MAX CARDS PER PERSONALITY ||=====
+
+#define MAX_KATANA_PER_PERS      1     
+#define MAX_SPEAR_PER_PERS       1
+#define MAX_BOW_PER_PERS         1
+#define MAX_NINJATO_PER_PERS     1
+#define MAX_WAKIZASHI_PER_PERS   1
+
+#define MAX_FOOTSOLDIER_PER_PERS 2
+#define MAX_ARCHER_PER_PERS      2
+#define MAX_SIEGER_PER_PERS      1
+#define MAX_CAVALRY_PER_PERS     2
+#define MAX_NAVAL_PER_PERS       1
+#define MAX_BUSHIDO_PER_PERS     1   
+
+
+//=====|| END OF FILE ||=====
