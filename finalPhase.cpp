d0976d7cb69c702d8b8ac362a87fef66b3992207
Harry <sdi1800105@di.uoa.gr>
2020-01-02 12:58:28 +0200

    finalPhase almost ready - 1 todo
    handling of MAX_HAND_CARDS define

--- finalPhase.cpp
+++ finalPhase.cpp
@@ -1,47 +1,58 @@
 //======|| GAME :: FINAL PHASE ||======
+//TODO: change the the handling of MAX_HAND_CARDS define
 #include <iostream>
 #include <vector>
+
 #include "baseClasses.h"
+#include "rules.hpp" // temp
 
-static void discardSurplusFateCards (Player * pl , size_t numOfCards) {
-  std::cout << "Player : " << pl->getUserName() << " has to discard "<< numOfCards <<" Fate Cards !" << std::endl;
+using std::cout;
+using std::endl;
+
+/* ========================================================================= */
+
+static void discardSurplusFateCards (Player * pl , size_t numOfCards)
+{ // could be a method of class Player, since MAX_HAND_CARDS *will* also be a field
+  while (numOfCards > 0)
+  {
+    cout << "Player : " << pl->getUserName() << " has to discard "
+         << numOfCards << " Fate Cards from their hand!" << endl;
+
+    cout << "\nPrinting hand!" << endl;
+    pl->printHand();
   
-    std::cout << "Type 'Y' (YES) or '<any other key>' (NO) after each card's \
-    appearance, to discard it." << std::endl;
+    cout << "\nType 'Y' (YES) or '<any other key>' (NO) after each card's \
+    appearance, to discard it." << endl;
 
-  for (auto * i : *(pl->getHand())) {
-    
-    if (numOfCards == 0) {
-      std::cout << "Discarded Surplus Fate Cards !" << std::endl;
-      return;
-    } 
+    for (auto card = pl->getHand()->begin(); card != pl->getHand()->end(); )
+    {
+      (*card)->print();
+      cout << "\nDiscard?\n> Your answer: ";
+      std::string answer;
+      std::getline(std::cin, answer);
+      cout << answer << endl;
 
-    i->print();
-    std::cout << std::endl <<"Discard ? "<< std::endl << "> Your answer: " ;
-    std::string answer;
-    std::getline(std::cin, answer);
-    std::cout << answer << std::endl;
-
-    if (answer == "Y") { 
-      pl->getHand()->pop_back();
-      std::cout << "Cards remaining : " << numOfCards-- << std::endl;
-    }
+      if (answer == "Y")
+      {
+        card = pl->getHand()->erase(card);
+        cout << "Cards still to discard: " << --numOfCards << endl;
+      }
+      else
+        ++card;
+    }  
   }
 }
+/* ========================================================================= */
 
-void Game::finalPhase(Player * pl) {
-
-    std::cout << "Final Phase Started !" << std::endl;
+void Game::finalPhase(Player * pl)
+{
+  cout << "Final Phase Started !" << endl;
 
   if (pl->getHand()->size() > MAX_HAND_CARDS)
     discardSurplusFateCards(pl , pl->getHand()->size() - MAX_HAND_CARDS);
 
-  // printGameStatistics
-  pl->printHand();
-  pl->printProvinces();
-  // printArena
-  pl->printHoldings();
+  cout << "Final Phase Ended !" << endl;
 
-  std::cout << "Final Phase Ended !" << std::endl;
-
+  printGameStatistics(); //[harry] everything about prints is included here
 }
+/* ========================================================================= */
