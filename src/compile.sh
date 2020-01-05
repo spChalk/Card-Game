fbb61ddccc2e7f11221b7906c70c039fdd657e9f
cMrzrs <sdi1800105@di.uoa.gr>
2020-01-05 22:38:01 +0200

    added warning flags

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #set -x
-g++ -o play -I. -I./Phases/ -I./Gameplay -I./Cards ./Cards/*.cpp ./Gameplay/*.cpp ./Phases/*.cpp ./*.cpp ../main.cpp
+g++ -Wall -Wextra -Werror -o play -I. -I./Phases/ -I./Gameplay -I./Cards ./Cards/*.cpp ./Gameplay/*.cpp ./Phases/*.cpp ./*.cpp ../main.cpp
